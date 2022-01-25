86edb3b630aa958ed967247f99e3be4300ce0734
Elthon Villa <91148072+elthonvilla@users.noreply.github.com>
2022-01-25 06:16:34 -0300

    Revert "Exercicios/10.1"

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
@@ -1,29 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import ReactClass from './FirstComponent';
 
 function App() {
-
-  // function helloWorld (nome, sobrenome) {
-  //   return <h1>Hello, {`${nome} ${sobrenome}`}</h1>;
-  // }
-
-  // const element = helloWorld("Jorge", "Maravilha");
-  // const container = <div>{element}</div>;
-
   const nome = 'Jorge Maravilha';
-  const classe = 'hello-class';
-  const element = <h1 className={classe}>Hello, {nome}</h1>;
-  const textJSX = <h1>Hello, JSX</h1>; // exercicio de fixacao FIX01
-
   return (
-
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {element}
-        {textJSX}
-        
+
+        <h1>Hello, {nome}</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -31,7 +20,6 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-          <ReactClass />
         </a>
       </header>
     </div>
